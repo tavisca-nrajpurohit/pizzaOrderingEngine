0e21b16e5c010df1588e3eb429b3cdffeb149f74
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-12-10 11:55:03 +0530

    built Pizza class

--- Pizza/Pizza.cs
+++ Pizza/Pizza.cs
@@ -1,10 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Pizza
 {
+    enum Topping
+    {
+        Capsicum,
+        Paneer,
+        Onion,
+        Chicken,
+        Corn,
+        Olives,
+        Mushrooms
+    };
+
+    enum PizzaSize
+    {
+        small,
+        medium,
+        large
+    };
+
     class Pizza
     {
+        public Boolean isVeg;
+        public Topping[] toppings;
+        public PizzaSize size;
+        public double price;
+
+        public Pizza(Boolean IsVeg, Topping[] Toppings, PizzaSize Size)
+        {
+            this.size = Size;
+            this.toppings = Toppings;
+            this.isVeg = IsVeg;
+            this.price = this.CalculatePrice();
+        }
+
+        public double CalculatePrice()
+        {
+            double price = 0;
+            foreach (var item in this.toppings)
+            {
+                price += 30;
+            }
+
+            switch (this.size)
+            {
+                case PizzaSize.small:
+                    price += 100;
+                    break;
+                case PizzaSize.medium:
+                    price += 200;
+                    break;
+                case PizzaSize.large:
+                    price += 300;
+                    break;
+                default:
+                    price += 200;
+                    break;
+            }
+
+            this.price = price;
+            return this.price;
+        }
     }
 }
