88f1673b14d327901601feda75037a3363d25af3
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-12-10 11:25:06 +0530

    Solution Setup

---
+++ Pizza/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
